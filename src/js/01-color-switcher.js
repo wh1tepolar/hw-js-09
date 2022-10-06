814b747ca5f8933fb52d0509b067aef63456e73b
whiteroma <roma.white95@gmail.com>
2022-10-06 11:51:24 +0200

    ю

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', () => {
+    const changeCol = () => document.body.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval(changeCol, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+
+
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(intervalId);
+});
